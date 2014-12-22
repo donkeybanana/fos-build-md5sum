4d7f9caec284278dcc96ce670fcc1e0b874d95d8
Alan Burgoyne <alan.burgoyne@frogasia.com>
2014-12-22 17:16:43 +0800

    Support both git and md5 methods of retrieving hashes

--- test/test.js
+++ test/test.js
@@ -3,18 +3,36 @@
 var assert = require('assert');
 var run = require('../');
 
-describe('fos-build-md5sum node module', function () {
+describe('fos-build-md5sum git', function () {
   it('must return HEAD for a given path', function () {
-    assert(run('test'), 'HEAD is defined');
+    assert(run.git('test'), 'HEAD is defined');
   });
 
   it('must return HEAD when no path is specified', function () {
-    assert(run(null), 'HEAD is defined');
+    assert(run.git(null), 'HEAD is defined');
   });
 
   it('must throw an error when an invalid path is specified', function () {
     try {
-      run('nopathtosee');
+      run.git('nopathtosee');
+    } catch (e) {
+      assert(true, 'Error thrown');
+    }
+  });
+});
+
+describe('fos-build-md5sum md5', function () {
+  it('must return hash for a given path', function () {
+    assert(run.md5('test'), 'hash is defined');
+  });
+
+  it('must return hash when no path is specified', function () {
+    assert(run.md5(null), 'hash is defined');
+  });
+
+  it('must throw an error when an invalid path is specified', function () {
+    try {
+      run.md5('nopathtosee');
     } catch (e) {
       assert(true, 'Error thrown');
     }
