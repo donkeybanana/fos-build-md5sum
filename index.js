36293df309689bb2a609972b4e80a5dca4058393
Alan Burgoyne <alan.burgoyne@frogasia.com>
2014-12-22 12:54:16 +0800

    Simple git log implementation to fetch the head for a given repo/path

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+var exec = require('child_process').exec;
+module.exports = function (path, next) {
+  var command = 'git log -n1 --pretty=oneline';
+
+  if (path) {
+    command += ' -- ' + path;
+  }
+
+  exec(command, function(err, stdout, stderr) {
+    var HEAD = stdout.split('\n')[0];
+    next(stderr || err, HEAD);
+  });
 };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
 /*global describe, it */
 'use strict';
 var assert = require('assert');
-var fosBuildMd5sum = require('../');
+var run = require('../');
 
 describe('fos-build-md5sum node module', function () {
-  it('must have at least one test', function () {
-    fosBuildMd5sum();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  it('must return HEAD for a given path', function () {
+    run('test', function(HEAD){
+        assert(HEAD, 'HEAD is defined');
+    });
+  });
+
+  it('must return HEAD when no path is specified', function () {
+    run(null, function(err, HEAD){
+        assert(HEAD, 'HEAD is defined');
+    });
   });
 });
