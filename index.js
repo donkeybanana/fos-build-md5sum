70a17f2981bafc39ca9b748f21f0f47a417c4fc4
Alan Burgoyne <alan.burgoyne@frogasia.com>
2019-04-05 15:35:49 +0800

    refactor: es6 class syntax

--- index.js
+++ index.js
@@ -1,68 +1,79 @@
 'use strict';
-var extend = require('util-extend');
-var hasha = require('hasha');
-var glob = require('glob');
-var fs = require('fs');
-var p = require('path');
+const extend = require('util-extend');
+const hasha = require('hasha');
+const glob = require('glob');
+const fs = require('fs');
+const p = require('path');
 
-var defaults = {
-  encoding: 'base64',
-  algorithm: 'md5',
-  pattern: '**/*.*',
-  dest: 'production.md5',
-  root: './'
-};
+module.exports = class BuildSumMD5 {
+  get options() {
+    return this.config;
+  }
+  set options(options = {}) {
+    this.config = {
+      encoding: 'base64',
+      algorithm: 'md5',
+      pattern: '**/*.*',
+      dest: 'production.md5',
+      root: './',
+      ...options
+    };
 
-function BuildSumMD5(options){
-  this.options(extend(defaults, options || {}));
-}
-
-BuildSumMD5.prototype.options = function(options) {
-  this.config = extend(this.config || {}, options || {});
-  return this.config;
-};
-
-BuildSumMD5.prototype.create_hash = function(data) {
-  return hasha(data, this.config);
-};
-
-BuildSumMD5.prototype.hash_file = function (path) {
-  var data = fs.readFileSync(path);
-  return this.create_hash(data);
-};
-
-BuildSumMD5.prototype.compare = function(file) {
-  var path = p.normalize(this.config.root + '/' + file + '/');
-  var oldsum = -1;
-
-  // Read current version
-  var newsum = this.read(file);
-
-  // Cached path
-  var pathsum = path + this.config.dest;
-
-  // Read cached version
-  if (fs.existsSync(pathsum)) {
-    oldsum = fs.readFileSync(pathsum).toString('utf-8').split('\n')[0];
+    return this.config;
   }
 
-  // Compare versions
-  return oldsum === newsum;
+  constructor(options = {}) {
+    this.config = options;
+  }
+
+  create_hash(data) {
+    return hasha(data, this.config);
+  }
+
+  hash_file(path) {
+    const data = fs.readFileSync(path);
+    return this.create_hash(data);
+  }
+
+  create_hash(data) {
+    return hasha(data, this.config);
+  }
+
+  compare(file) {
+    const path = p.normalize(this.config.root + '/' + file + '/');
+
+    // Read current version
+    const newsum = this.read(file);
+
+    // Cached path
+    const pathsum = path + this.options.dest;
+
+    // Read cached version
+    var oldsum = -1;
+    if (fs.existsSync(pathsum)) {
+      oldsum = fs
+        .readFileSync(pathsum)
+        .toString('utf-8')
+        .split('\n')[0];
+    }
+
+    // Compare versions
+    return oldsum === newsum;
+  }
+
+  read(file) {
+    const path = p.normalize(this.config.root + '/' + file + '/');
+    const files = glob.sync(path + this.config.pattern);
+
+    // Build matching files list
+    var manifest = {};
+    files.map(
+      function(f) {
+        manifest[f.replace(path, '')] = this.hash_file(f);
+      }.bind(this)
+    );
+
+    // Return hash of matching files list
+    return this.create_hash(JSON.stringify(manifest));
+  }
 };
-
-BuildSumMD5.prototype.read = function(file) {
-  var path = p.normalize(this.config.root + '/' + file + '/');
-
-  // Build matching files list
-  var manifest = {};
-  var files = glob.sync(path + this.config.pattern);
-
-  files.map(function(f){
-    manifest[f.replace(path,'')] = this.hash_file(f);
-  }.bind(this));
-
-  // Return hash of matching files list
-  return this.create_hash(JSON.stringify(manifest));
-};
-
-module.exports = BuildSumMD5;
